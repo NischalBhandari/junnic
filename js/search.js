b2e8c4a293712fac81a6066c208a62e95847bdc7
nischal <niscalbhandari12@gmail.com>
2019-12-24 20:02:30 +0545

    added signup responsiveness

--- js/search.js
+++ js/search.js
@@ -1,7 +1,11 @@
 var x = document.getElementById('show-search');
 var y = document.getElementById('search-bar');
 var closeSearch = document.getElementById('close-search');
+var mobileMenu = document.getElementsByClassName('mobile-menu');
+var navBar = document.getElementById('my-nav');
+var searchBar = document.getElementById('search-bar');
 var show = true;
+var boolmobMenu = true;
 
 x.onclick = function () {
     if (show) {
@@ -14,6 +18,18 @@ x.onclick = function () {
     }
 }
 closeSearch.onclick = function () {
-    y.style.display = 'none';
+   searchBar.style.display = 'none';
     show = !show;
+}
+console.log(mobileMenu[0]);
+mobileMenu[0].onclick = function () {
+    if (boolmobMenu) {
+        navBar.style.display = 'block';
+
+        boolmobMenu = !boolmobMenu;
+    }
+    else {
+        navBar.style.display = 'none';
+        boolmobMenu = !boolmobMenu;
+    }
 }
